78d342d8847c01670d33f620bac1dd912a0feaa4
Jonathan <minkhant5771@gmail.com>
2024-05-17 01:12:32 +0630

    wrong import error fixed

--- src/constants/user-testimonials.ts
+++ src/constants/user-testimonials.ts
@@ -1,6 +1,5 @@
 import { ReactNode } from "react";
 import { USER_IMAGES } from "./user-images.sources";
-import { FirstReview, SecondReview } from "./reviews";
 
 interface UserTestimonial {
   avatar: string;
