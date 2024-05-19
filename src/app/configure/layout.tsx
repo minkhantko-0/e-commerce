6920d23157cf6ca7bd6a286d9cb3a29a2c9822e4
Jonathan <minkhant5771@gmail.com>
2024-05-20 00:41:09 +0630

    steppr added and improved ui

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,9 +1,11 @@
 import MaxWidthWrapper from "@/components/max-width-warpper";
+import Steps from "@/components/steps";
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <MaxWidthWrapper className="flex-1 flex flex-col">
+      <Steps />
       {children}
     </MaxWidthWrapper>
   );
